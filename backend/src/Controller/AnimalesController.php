78c801e6ce8f9d8ec2c09ae7d43c16fd2a3d5aa9
Juan Urquiza <juanitourquiza@gmail.com>
2020-11-02 12:12:50 -0500

    cambio libreria

--- backend/src/Controller/AnimalesController.php
+++ backend/src/Controller/AnimalesController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Animales;
-use App\Form\animalesType;
+use App\Form\AnimalesType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
